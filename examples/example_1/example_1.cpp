a6bcf7fe1d1faded44f9fee88d4f938e6daace19
Jacopo Palumbo <jacopopalumbo@MacBook-Pro-di-Jacopo.local>
2023-11-17 12:16:20 +0100

    Added Particle class and first example

---
+++ examples/example_1/example_1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+#include "../../src/Particle/Particle.hpp"
+
+int main() {
+  NBodyEnv::Particle testParticle(NBodyEnv::gravitational, 1e10,
+                                  {0.0, 0.0, 0.0}, {1.0, 0.0, 0.0});
+
+  std::cout << "Example1 - Particle creation" << std::endl;
+
+  if (testParticle.getType() == 0) {
+    std::cout << "  Particle type: Gravitational" << std::endl;
+  } else {
+    std::cout << "  Particle type: Electromagnetic" << std::endl;
+  }
+
+  std::cout << "  Spec info: " << testParticle.getSpecInfo() << std::endl;
+  std::cout << "  Position: {" << testParticle.getPos().xPos << ", "
+            << testParticle.getPos().yPos << ", " << testParticle.getPos().zPos
+            << "}" << std::endl;
+  std::cout << "  Velocity: {" << testParticle.getVel().xVel << ", "
+            << testParticle.getVel().yVel << ", " << testParticle.getVel().zVel
+            << "}" << std::endl;
+
+  return 0;
+}
